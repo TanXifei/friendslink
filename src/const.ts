da181d2ae5871a507f4f775085744b32dda3b408
TanXifei <119285809+TanXifei@users.noreply.github.com>
2024-11-07 19:49:28 +0800

    Update const.ts

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'SofiDev | Links';
-export const SITE_DESCRIPTION = 'Frontend Developer. JavaScript Specialist';
+export const SITE_TITLE = '谭汐霏丨友链站';
+export const SITE_DESCRIPTION = '再见，世界！';
