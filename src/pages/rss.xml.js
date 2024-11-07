590d1b6f63dfad33285c1a742a015d599c8a8ed8
TanXifei <119285809+TanXifei@users.noreply.github.com>
2024-11-07 19:45:03 +0800

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'SofiDev | Links',
+    title: '谭汐霏丨友链站',
     description: 'All my Links',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
